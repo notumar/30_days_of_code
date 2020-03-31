c31fa70ece174a33943d086520f7e986ef79cf58
Nukeace <ahmedtijaniumar@.com>
2020-03-31 14:54:44 +0100

    Day One code

---
+++ python/Guessing Game/AhmedTijani_Umar_day1.py
@@ -0,0 +1,40 @@
+def guess_my_number(attempts,answer):
+    '''
+    takes a guess from a user and returns the difference between their final guess and the actual value
+    
+    Parameters
+    ----------
+    guess: int 
+    a value between 0 and 100
+    
+    attempts: int
+        the number of guesses you wish to allow
+    answer: int
+        The target number
+    '''
+    while attempts > 0:
+        guess = int(input("Please enter a number between 0 and 100: "))
+        if guess > 100 or guess < 0:
+            guess= int(input("Kindly enter a number between 0 and 100: "))
+            continue
+        else:
+            if guess == answer:
+                print(f"Youre right! The number was guess:{guess} and answer:{answer}")
+                break
+
+            elif guess > answer:
+                print ("guess lower")
+                attempts -=1
+                print(f"you have {attempts} attempts left")
+
+            elif guess < answer:
+                print ("guess higher")
+                attempts -=1
+                print(f"you have {attempts} attempts left")
+    else:
+        print(f"you have {attempts} attempts left... thanks for guessing!")
+
+
+
+
+
---
+++ python/Guessing Game/test.py
@@ -0,0 +1,5 @@
+word = 11
+
+while word != 20:
+    print(word)
+    word+=1
